9d108a51162193b652b23b8aaada8fa1a3dbcdbf
allonshore <411628468@qq.com>
2016-08-09 09:15:21 +0800

    --
    --

--- script/config.js
+++ script/config.js
@@ -1,20 +1,9 @@
 require.config({
-<<<<<<< HEAD
     baseUrl: "script/libs"
 
 });
 
 require(["swiper.min"],function(Swiper){
-=======
-    baseUrl: "script/libs",
-    paths: {
-        "jquery": "",
-        "swiper": "libs/swiper.min"
-    }
-});
-
-require([swiper],function(){
->>>>>>> origin/master
     var topSlider=new Swiper('.swiper-container',{
         sliderPerView:1,
         centeredSlides:true,
@@ -25,8 +14,4 @@ require([swiper],function(){
 });
 
 
-<<<<<<< HEAD
 //alert("加载成功！");
-=======
-alert("加载成功！");
->>>>>>> origin/master
